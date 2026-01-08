0b40a0581c6743f593ec28cc2fa9843955a3d672
Siddharth Jain 001 <158541181+siddhistan@users.noreply.github.com>
2026-01-08 14:20:25 +0530

    Update sorting.h

--- src/sorting.h
+++ src/sorting.h
@@ -1,1 +1,143 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+void bubble_sort(int a[], int n);
+void selection_sort(int a[], int n);
+void insertion_sort(int a[], int n);
+
+void merge_sort(int a[], int lb, int ub,int *comparisons, int * inversions);
+void merge(int a[], int lb, int mid, int ub,int* comparisons, int* inversions);
+
+void lomuto_quick_sort(int a[], int low, int high, int *comparisons, int* swaps);
+int lomuto_partition(int a[], int low, int high, int* comparisons, int* swaps);
+
+void hoare_quick_sort(int a[], int low, int high, int *comparisons, int* swaps);
+int hoare_partition(int a[], int low, int high, int* comparisons, int* swaps);
+
+void randomized_quick_sort(int a[], int low, int high, int *comparisons, int* swaps);
+int randomized_partition(int a[], int low, int high, int* comparisons, int* swaps);
+int partition(int a[],int low,int high,int*comparisons,int*swaps);
+
+void heap_sort(int a[], int n,int* comparisons, int * swaps);
+void build_max_heap(int a[], int n,int* comparisons, int * swaps);
+void heapify(int a[],int n,int i,int* comparisons, int * swaps);
+
+void swap(int *x, int *y);
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
