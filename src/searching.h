b6cc5a9ab75424503416d9efb50639ddd119b1f2
Siddharth Jain 001 <158541181+siddhistan@users.noreply.github.com>
2026-01-08 15:34:57 +0530

    Add search function declarations in searching.h

--- src/searching.h
+++ src/searching.h
@@ -1,1 +1,15 @@
+#ifndef SEARCHING_H
+#define SEARCHING_H
 
+void linear_search(int a[], int n,int key);
+
+int binary_search(int a[] , int n, int key);
+
+int recursive_linear_search(int*, int,int);
+
+int recursive_binary_search(int a[] , int low,int high, int key);
+
+
+
+
+#endif
